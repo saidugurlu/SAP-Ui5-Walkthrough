ad6389fe26b9a848217ad0672df991823260cb23
saidugurlu <ugurlumsaid@gmx.de>
2023-05-10 10:40:48 +0200

    MessageToast from sap.m library in

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,11 @@
-sap.ui.define([
-    'sap/ui/core/mvc/Controller',
-], function(Controller) {
-    'use strict';
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  function (Controller,MessageToast) {
+    "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onShowHello: function() {
-            // show a native JavaScript alert
-            alert("Hello World");
-        }
+      onShowHello: function () {
+        MessageToast.show("Hello there!");
+      },
     });
-    
-});
+  }
+);
