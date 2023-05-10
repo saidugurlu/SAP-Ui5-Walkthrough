8b9dfe73e699c077b5d5f4daf07b19f0cb2a3e43
saidugurlu <ugurlumsaid@gmx.de>
2023-05-10 14:28:55 +0200

    Component Configuration in

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,39 +1,16 @@
-sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  function (Controller, MessageToast, JSONModel, ResourceModel) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onInit: function () {
-        // set data model on view
-        let oData = {
-          recipient: {
-            name: "UI5",
-          },
-        };
-        let oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-
-        // set i18n model on view
-        let i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""], // supported locale array (all languages supported if empty array)
-          fallbackLocale: "", // fallback language (if no locale found, this language will be used)
-        });
-        this.getView().setModel(i18nModel, "i18n");
-      },
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+  "use strict"
+  return Controller.extend("sap.ui.demo.walkthrough.App", {
       onShowHello: function () {
-        // read msg from i18n model
-        let oBundle = this.getView().getModel("i18n").getResourceBundle();
-        let sRecipient = this.getView().getModel().getProperty("/recipient/name");
-        let sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        // Show message
-        MessageToast.show(sMsg);
-        },  
-    });
-    }
-);
+          // read msg from i18n model
+          var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+          // Show message
+          MessageToast.show(sMsg);
+      }
+  });
+});
