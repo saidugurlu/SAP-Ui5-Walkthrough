33f0cc60b5d2820b38ccef8f471015ad939d5940
saidugurlu <ugurlumsaid@gmx.de>
2023-05-12 11:24:40 +0200

    searchbar in

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -2,7 +2,8 @@ sap.ui.define([], function () {
 	"use strict";
 	return {
 		statusText: function (sStatus) {
-			var resourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			let 
+            resourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 			switch (sStatus) {
 				case "A":
 					return resourceBundle.getText("invoiceStatusA");
